3722535990dcf4d81a572166fa3a957fb77abd6a
GitHub Action <action@github.com>
2024-04-28 19:10:09 +0000

    Template cleanup

--- src/main/kotlin/com/github/blai30/galewindjetbrains/services/MyProjectService.kt
+++ src/main/kotlin/com/github/blai30/galewindjetbrains/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.blai30.galewindjetbrains.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.blai30.galewindjetbrains.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
